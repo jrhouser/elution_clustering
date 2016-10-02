71e16e4cd1f20baced381a6cd1348e4a6d9db085
John Houser <jrh94@marccomp01.ccbb.utexas.edu>
2016-10-02 11:21:54 -0500

    sanity checks

--- testdata/sanitychecks/generate_fake_elution_data.py
+++ testdata/sanitychecks/generate_fake_elution_data.py
@@ -6,22 +6,28 @@ import numpy as np
 def main():
 	
 	#generate 5 clusters of with random elution profiles
-	
+ 	clusters=[]	
 	#for a given cluster generate distributions
 	df=pd.DataFrame(columns=range(100))
+	q=0
 	for i in range(5):
-	
-		mu, sigma = np.random.uniform(0,100), np.random.uniform(0,10) # mean and standard deviation
-		for j in range(np.random.randint(5)):
+		cluster_n = ''
+		mu, sigma = np.random.uniform(20,80), np.random.uniform(0,20) # mean and standard deviation
+		for j in range(np.random.randint(5)+1):
 			sample = np.random.normal(mu,sigma,500)
 			sample = np.clip(sample,0,99)
+			sample = np.round(sample)
 			unique, counts = np.unique(sample,return_counts=True)
 			elution=np.zeros(100)
 			for k,u in enumerate(unique):
 				elution[u]=counts[k]
-			df.ix['p'+str(j)+'_cluster'+str(i)]=elution
+			df.ix['p'+str(q)+'_cluster'+str(i)]=elution
+			
+			cluster_n=cluster_n+'	' + 'p'+str(q)+'_cluster'+str(i)
+			q=q+1
+		clusters.append(cluster_n)
 
-
+	pd.DataFrame(clusters).to_csv('./test_clusters.txt',index=False,header=False)
 	df.to_csv('./test_elution.tab',sep='\t')
 	print df
 
